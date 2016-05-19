4bc3a46cd3cc0b557ae366a8d7973a9e51ea5e12
Jeff <jeffrey.l.turner@me.com>
2016-05-19 11:00:58 -0700

    updated w/MIT license

--- imdb-get.sh
+++ imdb-get.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # imdb-get gets current IMDB.COM data for a given studio with optional date range
-# Copyright (c) 2012 InterStream LLC All Rights Reserved.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 # outputs to stdout by default or filename as option 
 # Date range defaults to entire list from database if not specified
 # Will process up to 20,000 items / can set higher by change MAX env variable
